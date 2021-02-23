a088530bbf80f6adfff07666df012bc12be0d358
shubham <shubham.pol@apisero.com>
2021-02-24 01:15:48 +0530

    4 commit

--- src/test/java/matrixmult/MatMulTest.java
+++ src/test/java/matrixmult/MatMulTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import main.java.matrixmult.MatMulMain;
+import matrixmult.MatMulMain;
 public class MatMulTest {
 	
 	public static MatMulMain obj;
