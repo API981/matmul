978d779efa35040e10cdce1078be5b9538644adb
shubham <shubham.pol@apisero.com>
2021-02-24 00:39:37 +0530

    first commit

---
+++ src/main/java/matrixmult/MatMulMain.java
@@ -0,0 +1,48 @@
+package matrixmult;
+
+public class MatMulMain {
+
+	
+	int[][] mult(int[][] mat1,int[][] mat2) 
+	{ 
+		if(mat1[0].length!=mat2.length)
+		{
+			throw new RuntimeException("Matrix multiplication not possible");
+		}
+		else {
+	int[][] res = new int[mat1.length][mat2[0].length]; 
+	for(int i=0;i<mat1.length;i++) 
+	{ 
+	for(int j=0;j<mat2[0].length;j++) 
+	{ 
+		for (int k = 0; k < mat2.length; k++) 
+			res[i][j]+=mat1[i][k]*mat2[k][j]; } 
+	} 
+	 return res;
+		}
+	} 
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int A[][] = { { 1, 1, 1 }, 
+                { 2, 2, 2 }, 
+                { 3, 3, 3 }, 
+                { 4, 4, 4 } }; 
+
+	  int B[][] = { { 1, 1, 1, 1 }, 
+	                { 2, 2, 2, 2 }, 
+	                { 3, 3, 3, 3 } }; 
+	  
+	  MatMulMain obj = new MatMulMain();
+	  int res[][]=obj.mult(A, B);
+	  for(int i=0;i<res.length;i++) 
+		{ 
+		for(int j=0;j<res[i].length;j++) 
+		{ 
+		System.out.print(res[i][j]+" "); 
+		} 
+		System.out.println(); 
+		}
+	}
+
+}
---
+++ src/test/java/matrixmult/MatMulTest.java
@@ -0,0 +1,55 @@
+package matrixmult;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class MatMulTest {
+	
+	public static MatMulMain obj;
+	@BeforeAll
+	public static void CreateObj()
+	{
+		obj=new MatMulMain();
+	}
+	int A[][] = { { 1, 1, 1 }, 
+            { 2, 2, 2 }, 
+            { 3, 3, 3 }, 
+            { 4, 4, 4 } }; 
+
+	int B[][] = { { 1, 1, 1, 1 }, 
+                { 2, 2, 2, 2 }, 
+                { 3, 3, 3, 3 },
+                { 4, 4, 4, 4 }}; 
+	@Test
+	public void testrowcol()
+	{
+		try{
+			obj.mult(A, B);
+		}
+		catch (Exception e) {
+			// TODO: handle exception
+			Assertions.assertEquals("Matrix multiplication not possible", e.getMessage());
+		}
+	}
+	int C[][]= { { 1, 1, 1, 1 }, 
+            { 2, 2, 2, 2 }, 
+            { 3, 3, 3, 3 }}; 
+	
+	int res[][]= {{6,6,6,6}, 
+			{12,12,12,12},
+			{18,18,18,18}, 
+			{24,24,24,24}};
+	@Test
+	public void testvalid()
+	{
+		Assertions.assertArrayEquals(res, obj.mult(A, C));
+	}
+	
+/*	@Test
+	public void testinvalid()
+	{
+		res[0][3]=8;
+		Assertions.assertArrayEquals(res, obj.mult(A, C));
+	}*/
+}
